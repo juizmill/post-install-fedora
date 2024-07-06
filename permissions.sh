c0764e3936a17494e2e86f08b78d152ee469c49b
Jesus Vieira de Lima <jesusvieiradelima@gmail.com>
2024-07-06 18:25:29 -0400

    Update permissions.sh
    Removendo bug na linha 1 e add a variavel $HOME

--- permissions.sh
+++ permissions.sh
@@ -1,7 +1,7 @@
-#!/bim/bash
+#!/bin/bash
 
-mkdir -p ~/.ssh
-chmod 400 *.pem ~/
-chmod 700 ~/.ssh
-chmod 644 ~/.ssh/*.pub
-find ~/.ssh -type f -not -name "*.pub" -exec chmod 600 \{\} \;
+mkdir -p $HOME/.ssh
+chmod 400 $HOME/*.pem
+chmod 700 $HOME/.ssh
+chmod 644 $HOME/.ssh/*.pub
+find $HOME/.ssh -type f -not -name "*.pub" -exec chmod 600 \{\} \;
